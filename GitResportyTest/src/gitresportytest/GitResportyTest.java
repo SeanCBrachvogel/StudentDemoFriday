47eca2284474e8f2015495f90b844cd904a92763
Sean <brachvog@sheridancollege.ca.>
2019-03-07 21:30:57 -0500

    added git restest

---
+++ GitResportyTest/src/gitresportytest/GitResportyTest.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gitresportytest;
+
+/**
+ *
+ * @author seanw
+ */
+public class GitResportyTest {
+
+    public String test2 = "0";
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
